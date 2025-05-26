afb42b36369c95f7df8ffc1fac0020e86e0cde0d
Osama Javed <osamajaved2002@gmail.com>
2025-05-27 03:09:09 +0500

    27-05-2025

--- src/components/other/Header.jsx
+++ src/components/other/Header.jsx
@@ -19,7 +19,7 @@ const Header = (props) => {
   return (
     <div className="flex items-end justify-between">
       <h1 className="text-2xl font-medium">
-        Hello <br /> <span className="text-3xl font-semibold">username 👋</span>
+        Hello <br /> <span className="text-3xl font-semibold">Username 👋</span>
       </h1>
       <button onClick={logOutUser} className="bg-red-600 text-base font-medium text-white px-5 py-2 rounded-sm">
         Log Out
